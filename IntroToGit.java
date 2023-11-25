b079e80d03955ee9dc4e17b18c577dd44fc26231
MohAbdalla18 <mabdalla9084@students.mc3.edu>
2023-11-25 15:33:36 -0500

    Create IntroToGit.java

---
+++ IntroToGit.java
@@ -0,0 +1,16 @@
+public class IntroToGit
+{
+ public static void main(String[] args){
+ 
+ System.out.println("testing Github");
+ 
+ int grade = 93;
+ 
+ if (grade<65){
+ System.out.println("fail");
+ }
+ else{
+ System.out.println("Pass");
+ }
+ }
+ }
